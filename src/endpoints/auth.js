131875ca2965fd710b58c91029fd44b13b2cc1fd
Isaac Krementsov <isaackrementsov@gmail.com>
2018-05-02 13:51:52 -0400

    Signup and update methods removed (they can be handled by ConDev)

--- src/endpoints/auth.js
+++ src/endpoints/auth.js
@@ -17,39 +17,9 @@ module.exports = {
             res.redirect('/login')
         }
     },
-    renderSignup: function(req, res) {
-        res.render('signup', {session:req.session});
-
-    },
-    signup: function(req, res) {
-        var User = require('../models/user.js');
-        var dev;
-        if(req.body.dev == 'true') {
-            var dev = true
-        } else {
-            var dev = false
-        }
-        dbCreate.newUser({username:req.body.username.trim(), password: req.body.password.trim(), dev:dev, createdAt:Date.now()}, function(err, saved) {
-            if(err) {
-                req.session.err = ['That username is already taken, please choose a different one!']
-                res.redirect('/signup')
-            } else {
-                module.exports.login(req, res)
-            }
-        });
-    },
     logout: function(req, res) {
         req.session.destroy();
         res.redirect('/login')
-    },
-    update: function(req, res) {
-        if(req.parrams.attr == 'lang' && req.session.dev) {
-            dbUpdate.updateUser({'username':req.session.user}, {$push:{'languages':{'name':req.body.language}}});
-        } else if(req.params.attr == 'bio') {
-            dbUpdate.updateUser({'username':req.session.user}, {'bio':req.body.description})
-        }
-        var dev = req.session.dev ? 'devs' : 'clients';
-        res.redirect('/' + dev + '/' + req.session.user)
     },
     any: function(req, res) {
         res.render('404', {session:req.session});
