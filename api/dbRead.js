20fadd381511fdfe38d3eff0fccae77be4bb7b41
Isaac Krementsov <isaackrementsov@gmail.com>
2018-05-12 15:08:21 -0400

    files deleted for merge

--- api/dbRead.js
+++
@@ -1,9 +0,0 @@
-var User = require('.models/user.js');
-
-var findUser = function(data, select, callback) {
-    return User.findOne(data, select, function(err, docs) {
-        if(err) {
-            
-        }
-    })
-}
