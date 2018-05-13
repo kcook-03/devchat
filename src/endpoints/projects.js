76a269cd96f708fb0b32a7bcc3a6bb56e703001f
Isaac Krementsov <isaackrementsov@gmail.com>
2018-05-13 11:21:37 -0400

    Styling tweaks and bug fixes

--- src/endpoints/projects.js
+++ src/endpoints/projects.js
@@ -3,19 +3,10 @@ var ObjectID = require('mongodb').ObjectID;
 var dbUpdate = require('../core/dbUpdate');
 module.exports = {
     show: async function(req,res){
-        var website = await dbFind.find('Website', {'_id':req.params.websiteId, 'closed':true});
-        if(website){
-            if(website.members.map(function(member){return member.name}).indexOf(req.session.user) != -1 || req.session.user == website.author){
-                var tasks = await dbFind.search('Task', {'websiteId':req.params.websiteId});
-                var jobs = await dbFind.search('Job', {'websiteId':req.params.websiteId});
-                req.session.chatId = req.params.websiteId;
-                res.render('project', {session:req.session, website:website, jobs:jobs, tasks:tasks})
-            }else{
-                res.redirect('/home')
-            }
-        }else{
-            res.redirect('/home')
-        }
+        var website = await dbFind.find('Website', {'_id':req.params.websiteId, 'closed':true, $or:[{'author':req.session.user, 'members.name':req.session.user}]});
+        var tasks = await dbFind.search('Task', {'websiteId':req.params.websiteId});
+        req.session.chatId = req.params.websiteId;
+        res.render('project', {session:req.session, website:website, tasks:tasks})
     },
     update: function(req,res){
         var websiteId = ObjectID(req.params.websiteId);
